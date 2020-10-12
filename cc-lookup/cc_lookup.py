0f2314e1a580e93c88eb722aac0dc3ec9745fe4c
Govarthanan <grajappa@tibco.com>
2020-10-11 21:09:33 -0500

    Added Monitoring for CCLookup Service

--- cc-lookup/cc_lookup.py
+++ cc-lookup/cc_lookup.py
@@ -4,7 +4,7 @@ from flask_api import status
 from lookup import apiConvert, apiDiag, health
 import prometheus_client
 from prometheus_client.core import CollectorRegistry
-from prometheus_client import Counter, Summary
+from prometheus_client import Counter, Histogram
 import time
 app = Flask(__name__)
 api = Api(app)
@@ -13,16 +13,19 @@ class CCLHealth(Resource):
     def get(self):
         ret = health()
         if ret == 200:
-            graphs['c'].inc()
             return "Ok", status.HTTP_200_OK
         else:
             return "Unhealthy", status.HTTP_404_NOT_FOUND
 class CCLConvert(Resource):
     def post(self, cc):
+        start = time.time()
+        gdict['CCRequests'].inc()
         ret = apiConvert(cc)
         if ret == 'Country code not valid':
             return "Failed to retrieve Country", status.HTTP_406_NOT_ACCEPTABLE
         else: 
+            end = time.time()
+            gdict['CCRequests_hist'].observe(end - start)
             return ret, status.HTTP_200_OK
 class CCLDiag(Resource):
     def get(self):
@@ -35,12 +38,14 @@ class CCLDiag(Resource):
 class CCLMetrics(Resource):
     def get(self):
         res = []
-        for k,v in graphs.items():
+        for k,v in gdict.items():
             res.append(prometheus_client.generate_latest(v))
         return Response(res,mimetype="text/plain")
 
-graphs = {}
-graphs['c'] = Counter('requests_operation_total','The total number of processed requests')
+gdict = {}
+gdict['CCRequests'] = Counter('convert_requests_total','The total number of Country Code Convert requests')
+gdict['CCRequests_hist'] = Histogram('convert_request_duration_seconds','Historgram for the duration in seconds', buckets=(1,2,3,4,5,float('inf')))
+
 
 api.add_resource(CCLHealth, "/health")
 api.add_resource(CCLConvert, "/convert/<string:cc>")
